8536d2e247380db9cf075a91fdb8026d8b5e6121
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-21 21:38:42 -0600

    Fixing Navbar sticky and menu colors

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoggedService } from '../../services/logged.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class NavbarComponent implements OnInit {
 
-  constructor(private myRouter:Router, private logService:LoggedService) { 
-    
+  constructor(private myRouter:Router, private logService:LoggedService) {
+
     this.logService.loginStatus$.subscribe( value => {
       this.loginStatus = value;
     })
   }
-  
+
   ngOnInit(): void {
   }
-  
+
   thenBlock:TemplateRef<any>|null = null;
   elseBlock:TemplateRef<any>|null = null;
-  
+
   public loginStatus:any;
 
-  
+
 
   goLogin() {
     this.myRouter.navigate(['login']);
@@ -35,11 +36,11 @@ export class NavbarComponent implements OnInit {
     this.myRouter.navigate(['']);
     this.logService.loginStatus$.next(false);
   }
-  
+
   async getCategoryList() {
     return await this.myRouter.navigate(['recetas']);
   }
-  
+
 
 }
 
