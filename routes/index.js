9f345e1efc6ab95cc12b4e477cac0ffd7a294c40
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-28 12:35:56 -0600

    Modify appjs to set dist path

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(path.join(__dirname + '/dist/client/index.html'))
+router.get('/*', function(req, res, next) {
+  res.send(path.join(__dirname + '/client/dist/index.html'))
 
 })
 
