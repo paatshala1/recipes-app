1cfc7de0e6e0719da4202139d99b313bd132bfdb
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-28 10:21:58 -0600

    Modifications to deploy in Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/*', function(req, res, next) {
+  res.send(path.join(__dirname + '/dist/client/index.html'))
+
+})
 
 
 module.exports = router;
