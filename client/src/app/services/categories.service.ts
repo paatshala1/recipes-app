cb9d0645d91a5067b47fc80c70385f2073456586
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-29 19:52:03 -0600

    Update category service with heroku URL

--- client/src/app/services/categories.service.ts
+++ client/src/app/services/categories.service.ts
@@ -11,7 +11,7 @@ export class CategoriesService {
 
   constructor(private http:HttpClient) { }
 
-  URL_API = 'http://localhost:4000/api/category';
+  URL_API = 'https://angelacocina.herokuapp.com/api/category';
   canDelete!:boolean;
 
 
