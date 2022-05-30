2346988faacf119737c946dc5f50ffbd7c480474
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-29 21:57:10 -0600

    More changes to verify server functionality

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/*', function(req, res, next) {
-  res.send(path.join(__dirname + '/index.html'))
+  res.send(path.join(__dirname + 'index.html'))
   // res.send(path.join(__dirname + '/angela-kitchen-app/index.html'))
 
 })
