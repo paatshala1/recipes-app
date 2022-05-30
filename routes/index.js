4d9aed636eebc6d9591d9a735714ec0e23d030e0
paatshala1 <hugo.rodriguezz@icloud.com>
2022-05-29 22:00:27 -0600

    removing asterik to test server

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send(path.join(__dirname + 'index.html'))
   // res.send(path.join(__dirname + '/angela-kitchen-app/index.html'))
 
